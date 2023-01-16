409ad69f4cad93ce08457172bf6afe733ec4d9c8
Titus Wormer <tituswormer@gmail.com>
2023-01-16 09:15:08 +0100

    Replace `complex-types.d.ts` with `index.d.ts`

--- index.d.ts
+++ index.d.ts
@@ -1,11 +1,15 @@
 import type {Literal} from 'hast'
 
+export type {Options} from './lib/index.js'
+export {raw} from './lib/index.js'
+
 /* eslint-disable @typescript-eslint/consistent-type-definitions */
 
 export interface Raw extends Literal {
   type: 'raw'
 }
 
+// Add `Raw` nodes to roots and elements.
 declare module 'hast' {
   interface RootContentMap {
     raw: Raw
--- index.js
+++ index.js
@@ -1,6 +1,2 @@
-/**
- * @typedef {import('./lib/index.js').Options} Options
- * @typedef {import('./complex-types.js').Raw} Raw
- */
-
+// Note: see `index.d.ts` for exposed types.
 export {raw} from './lib/index.js'
