b83ec5fb50d9f673454754d2c088569f64b4e529
Titus Wormer <tituswormer@gmail.com>
2023-01-16 13:15:15 +0100

    Fix to reexport `Raw` from `mdast-util-to-hast`
    Previously a new raw was exposed, which was a different interface
    than the one from `mdast-util-to-hast`.
    That can be considered the canonical place where this extension
    comes from, so defer to that.

--- index.d.ts
+++ index.d.ts
@@ -1,23 +1,3 @@
-import type {Literal} from 'hast'
-
+export type {Raw} from 'mdast-util-to-hast'
 export type {Options} from './lib/index.js'
 export {raw} from './lib/index.js'
-
-/* eslint-disable @typescript-eslint/consistent-type-definitions */
-
-export interface Raw extends Literal {
-  type: 'raw'
-}
-
-// Add `Raw` nodes to roots and elements.
-declare module 'hast' {
-  interface RootContentMap {
-    raw: Raw
-  }
-
-  interface ElementContentMap {
-    raw: Raw
-  }
-}
-
-/* eslint-enable @typescript-eslint/consistent-type-definitions */
